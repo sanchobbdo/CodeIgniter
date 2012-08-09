4de0b3f7982b7077937fcec7172d12312bb9ba3c
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2012-08-09 09:24:35 -0500

    Added HMVC.

--- application/config/config.php
+++ application/config/config.php
@@ -357,6 +357,14 @@ $config['rewrite_short_tags'] = FALSE;
 */
 $config['proxy_ips'] = '';
 
+/*
+|--------------------------------------------------------------------------
+| HMVC Configuration
+|--------------------------------------------------------------------------
+*/
+$config['modules_locations'] = array(
+	APPPATH.'modules/' => '../modules/',
+);
 
 /* End of file config.php */
 /* Location: ./application/config/config.php */
---
+++ application/libraries/MY_Form_validation.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * MY_Form_validation
+ *
+ * @author Camilo Aguilar
+ * @package CodeIgniter
+ */
+
+class MY_Form_validation extends CI_Form_validation {
+	public $CI;
+}
+// END MY Form Validation class.
+
+/* End of file MY_Form_validation.php */
+/* Location: ./application/libraries/MY_Form_validation.php */
